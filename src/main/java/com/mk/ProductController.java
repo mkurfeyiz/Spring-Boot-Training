7a463bc74a88bb0f8ba2f92617c53d536a668e90
mkurfeyiz <m.kurfeyiz98@gmail.com>
2021-10-07 18:12:57 +0300

    Fixed url names.

--- src/main/java/com/mk/ProductController.java
+++ src/main/java/com/mk/ProductController.java
@@ -4,44 +4,47 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("product")
 public class ProductController {
 
     List<String> products;
 
     public ProductController(List<String> products){
         this.products = products;
+        this.products.add("tablet");
+        this.products.add("telefon");
+        this.products.add("bilgisayar");
     }
 
-    @GetMapping("getProducts")
+    @GetMapping("products")
     public List<String> getProducts(){
         return products;
     }
 
-    @GetMapping("getProductByIndex")
+    @GetMapping("products-by-index")
     public String getProductByIndex(@RequestParam int index){
         return products.get(index);
     }
 
-    @GetMapping("getProductByName")
+    @GetMapping("products-by-name")
     public String getProductByName(@RequestParam String product){
         int index = products.indexOf(product);
         return products.get(index);
     }
 
-    @PostMapping("addProduct")
+    @PostMapping("products")
     public void addProduct(@RequestBody String product){
         products.add(product);
     }
 
     //This makes path variables to be optional
     //@PutMapping(value = {"updateProduct", "updateProduct/{index}/{product}"})
-    @PutMapping("updateProduct/{index}/{product}")
+    @PutMapping("products/{index}/{product}")
     public void updateProduct(@PathVariable("index") int index, @PathVariable("product") String product){
         products.set(index, product);
     }
 
-    @DeleteMapping(value = {"deleteProduct", "deleteProduct/{index}"})
+    @DeleteMapping(value = {"products", "products/{index}"})
     public void deleteProduct(@PathVariable(required = false) int index){
         products.remove(index-1);
     }
